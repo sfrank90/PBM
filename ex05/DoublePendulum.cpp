21ff6d719ca394e686e8ada05c7c3823be352315
Sven Frank <sven.frank90@gmail.com>
2013-11-28 19:51:59 +0100

    preps for 5.2

--- ex05/DoublePendulum.cpp
+++ ex05/DoublePendulum.cpp
@@ -7,5 +7,16 @@ void DoublePendulum::computeAccelerations() {
 	/* Read the angle ("position") and angular velocity ("velocity") of both particles from the "particles" vector.
 	 * Then compute the angular accelerations and store them in the particles' "acceleration" variable.
 	 */
+
+	AngularVelocity p1_v, p2_v;
+	Length3D p1_p, p2_p; //angular position?!
+	for (std::vector<particle_type>::iterator p = particles.begin(), int i = 1; p != particles.end(); ++p, i++)
+	{
+		// read and store position and velocity
+		
+		// compute angular acceleration
+
+		// store acceleration for each particle
+	}
 }
 
